7e981f26f557f3d00de1d3f60959d0092dc9af1d
drngsl <drngsl@qq.com>
2018-08-17 12:06:03 +0800

    Construct userInfo map

--- src/main/java/com/huawei/imc/controller/UserController.java
+++ src/main/java/com/huawei/imc/controller/UserController.java
@@ -3,19 +3,22 @@
  */
 package com.huawei.imc.controller;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.log4j.Logger;
 import org.jasig.cas.client.authentication.AttributePrincipal;
 import org.jasig.cas.client.validation.AssertionImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author dengshaolin
@@ -24,7 +27,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
-	private static final Logger logger = Logger.getLogger(UserController.class);
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	private static final String CONST_CAS_ASSERTION = "_const_cas_assertion_";
 	private static final String KEY_USER_ID = "id";
@@ -46,7 +49,12 @@ public class UserController {
 			response.setStatus(HttpStatus.UNAUTHORIZED.value(), "invalid AttributePrincipal");
 			return null;
 		}
-		Map<String, Object> userInfo = principal.getAttributes();
+
+		Map<String, Object> userInfo = new HashMap<String, Object>();
+		Map<String, Object> principalAttr = principal.getAttributes();
+		for (Map.Entry<String, Object> entry : principalAttr.entrySet()) {
+			userInfo.put(entry.getKey(), entry.getValue());
+		}
 		String name = principal.getName();
 		if (!userInfo.containsKey(KEY_USER_ID)) {
 			userInfo.put(KEY_USER_ID, name);
