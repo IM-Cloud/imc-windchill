81b39b82ce6abde8b125981c3aa4fd3fe909c20c
wanggongyin <1286781598@qq.com>
2018-08-17 09:54:39 +0800

    添加服务查询

--- src/main/webapp/src/app/business/services/windchillService.js
+++ src/main/webapp/src/app/business/services/windchillService.js
@@ -1,13 +1,13 @@
 'use strict';
 angular.module('windchillApp').service('windchillService', ['$http', function ($http) {
     this.recentlyVisitedUrl = window.configData.appPath + '/windchill/recentlyVisited';
-    this.myPDMLinkProductsUrl = window.configData.appPath + "/Windchill/myPDMLinkProducts";
-    this.myCheckOutUrl = window.configData.appPath + "/Windchill/myCheckOut";
+    this.myPDMLinkProductsUrl = window.configData.appPath + "/windchill/myPDMLinkProducts";
+    this.myCheckOutUrl = window.configData.appPath + "/windchill/myCheckOut";
     this.getRecentlyList = function (params) {
         if (params) {
             this.recentlyVisitedUrl = this.recentlyVisitedUrl + '?';
             for (var key in params) {
-                this.recentlyVisitedUrl = this.recentlyVisitedUrl + key + '=' + params.key + '&';
+                this.recentlyVisitedUrl = this.recentlyVisitedUrl + key + '=' + params[key] + '&';
             }
             if (this.recentlyVisitedUrl.lastIndexOf('&')) {
                 this.recentlyVisitedUrl = this.recentlyVisitedUrl.substring(0, this.recentlyVisitedUrl.lastIndexOf('&'))
@@ -19,7 +19,7 @@ angular.module('windchillApp').service('windchillService', ['$http', function ($
         if (params) {
             this.myPDMLinkProductsUrl = this.myPDMLinkProductsUrl + '?';
             for (var key in params) {
-                this.myPDMLinkProductsUrl = this.myPDMLinkProductsUrl + key + '=' + params.key + '&';
+                this.myPDMLinkProductsUrl = this.myPDMLinkProductsUrl + key + '=' + params[key] + '&';
             }
             if (this.myPDMLinkProductsUrl.lastIndexOf('&')) {
                 this.myPDMLinkProductsUrl = this.myPDMLinkProductsUrl.substring(0, this.myPDMLinkProductsUrl.lastIndexOf('&'))
@@ -31,7 +31,7 @@ angular.module('windchillApp').service('windchillService', ['$http', function ($
         if (params) {
             this.myCheckOutUrl = this.myCheckOutUrl + '?';
             for (var key in params) {
-                this.myCheckOutUrl = this.myCheckOutUrl + key + '=' + params.key + '&';
+                this.myCheckOutUrl = this.myCheckOutUrl + key + '=' + params[key] + '&';
             }
             if (this.myCheckOutUrl.lastIndexOf('&')) {
                 this.myCheckOutUrl = this.myCheckOutUrl.substring(0, this.myCheckOutUrl.lastIndexOf('&'))
